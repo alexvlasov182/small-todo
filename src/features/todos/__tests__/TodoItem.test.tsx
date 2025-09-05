e4538969e2eead6a5e4c69b6c92928562260a2b1
Oleksandr Vlasov <drumlife182@gmail.com>
2025-09-05 14:58:27 +0200

    fix: tests

--- src/features/todos/__tests__/TodoItem.test.tsx
+++ src/features/todos/__tests__/TodoItem.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import { TodoItem } from '../components/TodoItem'
 import type { Todo } from '../types'
 
